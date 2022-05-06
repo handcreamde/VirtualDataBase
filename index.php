732920cad3b82e433e26c426c8d1bdc49d1a871a
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-05-06 22:27:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ function vdbinput($name1, $name2)
   copy('virtualdb/template.php', "virtualdb/db-".$name1.".php");
   file_put_contents("virtualdb/db-".$name1.".php", $name2, FILE_APPEND);
 }
-vdbinput("test", "test");
+vdbinput("name", "naiyou");
 ?>
