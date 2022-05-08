3f6fdf0c58e4c913d9480553a6327a92a71b4346
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-05-08 19:13:04 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,12 @@ function vdbinput($name1, $name2)
   copy('virtualdb/template.php', "virtualdb/db-".$name1.".php");
   file_put_contents("virtualdb/db-".$name1.".php", $name2, FILE_APPEND);
 }
-vdbinput("name", "naiyou");
+
+function vdbdelete($name1)
+{
+  unlink('virtualdb/db-'.$name1.'.php');
+}
+
+vdbinput("testname", "test");
+vdbdelete("testname");
 ?>
